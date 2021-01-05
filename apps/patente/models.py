62d7208a8065c7ead21484202446dcefc6e5bb00
Anderson Pozo <imbander092098@gmail.com>
2021-01-05 17:30:49 -0500

    Agregado modelos de plusvalia, patente y predio

---
+++ apps/patente/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+from apps.contribuyente.models import Contribuyente
+from apps.establecimiento.models import Establecimiento
+
+
+# Create your models here.
+class Patente(models.Model):
+    """
+    Modelo de Pantente que contiene los datos de la patente
+    municipal y su relación con el establecimiento
+    """
+    id = models.AutoField(primary_key=True)
+    numero_patente = models.IntegerField('Número de patente', blank=True, null=True)
+    exonerada = models.BooleanField('Patente exonerada', blank=True, null=True, default=False)
+    suspendida = models.BooleanField('Patente suspendida', blank=True, null=True, default=False)
+    contribuyente = models.ForeignKey(Contribuyente, on_delete=models.CASCADE)
+    establecimiento = models.ForeignKey(Establecimiento, on_delete=models.CASCADE)
+
+
+class DetallePatente(models.Model):
+    """
+    El modelo DetallePatente almacena un historico de los
+    movimientos de patente, ya se apertura o renovación
+    """
+    id = models.AutoField(primary_key=True)
+    zona = models.CharField('Zona urbana o rural', max_length=10, blank=True, null=True)
+    fecha = models.DateField('Fecha de tramite', blank=True, null=True)
+    impuesto = models.FloatField('Impuesto', blank=True, null=True)
+    interes = models.FloatField('Interes', blank=True, null=True)
+    multa = models.FloatField('Multa', blank=True, null=True)
+    servicios_administrativos = models.FloatField('Servicios administrativos', blank=True, null=True)
+    patente = models.ForeignKey(Patente, on_delete=models.CASCADE)
---
+++ apps/plusvalia/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class PlusvaliaConfig(AppConfig):
+    name = 'plusvalia'
