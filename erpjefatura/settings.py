4f3859f6b5dc29724977508226832bdf9419d79d
Anderson Pozo <imbander092098@gmail.com>
2021-01-04 19:00:59 -0500

    Fixed settings apps name

--- erpjefatura/settings.py
+++ erpjefatura/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'apps.contribuyente',
     'apps.direccion',
-    'apps.establecimiento'
+    'apps.establecimiento',
     'apps.impuesto',
 ]
 
