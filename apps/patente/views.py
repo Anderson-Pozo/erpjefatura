bbcc69de35efb861eb24147f703e3a5be1b80ecd
Anderson Pozo <imbander092098@gmail.com>
2021-03-01 22:21:10 -0500

    Fixed estado patente

--- apps/patente/views.py
+++ apps/patente/views.py
@@ -1,9 +1,8 @@
-from django.contrib import messages
 from django.shortcuts import redirect, render
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 from django.urls import reverse_lazy
-from django.views.generic import ListView, TemplateView, CreateView, View
+from django.views.generic import ListView, TemplateView, CreateView, View, UpdateView
 from django.http import HttpResponse, JsonResponse, HttpResponseRedirect
 from apps.contribuyente.models import Natural, Juridico, Contribuyente
 from apps.establecimiento.models import Establecimiento
@@ -13,11 +12,8 @@ from apps.utils.ajax import AjaxList
 from .models import Patente, DetallePatente
 from .forms import PatenteForm, DetalleForm
 
-import os
-from django.conf import settings
 from django.template.loader import get_template
 from xhtml2pdf import pisa
-from django.contrib.staticfiles import finders
 
 
 class ListaCatastro(AjaxList, ListView):
@@ -27,20 +23,6 @@ class ListaCatastro(AjaxList, ListView):
     @method_decorator(csrf_exempt)
     def dispatch(self, request, *args, **kwargs):
         return super().dispatch(request, *args, *kwargs)
-
-    # def post(self, request, *args, **kwargs):
-    #     data = {}
-    #     try:
-    #         action = request.POST['action']
-    #         if action == 'searchdata':
-    #             data = []
-    #             for i in Patente.objects.all():
-    #                 data.append(i.to_json())
-    #         else:
-    #             data['error'] = 'Ha ocurrido un error'
-    #     except Exception as e:
-    #         data['error'] = str(e)
-    #     return JsonResponse(data, safe=False)
 
 
 class CrearContribuyente(TemplateView):
@@ -53,31 +35,6 @@ class CrearContribuyente(TemplateView):
         context['form_natural'] = form_natural
         context['form_juridico'] = form_juridico
         return self.render_to_response(context)
-
-    # def post(self, request, *args, **kwargs):
-    #
-    #     form_natural = NaturalForm(request.POST, prefix="form_natural")
-    #     if form_natural.is_valid():
-    #         print(form_natural)
-    #         form_natural.save()
-    #         messages.success(request, 'Registro creado')
-    #         return redirect('patente:crear_contribuyente')
-    #     else:
-    #         return None
-
-        # context = self.get_context_data(**kwargs)
-        #
-        # if context['form_natural'].is_valid():
-        #     instance = NaturalForm.save(request.POST)
-        #     messages.success(request, 'Your offer bid #{0} has been submitted.'.format(instance.pk))
-        # elif context['form_juridico'].is_valid():
-        #     instance = context['form_juridico'].save()
-        #     messages.success(request, 'Your offer attachment #{0} has been submitted.'.format(instance.pk))
-        #     # advise of any errors
-        # else:
-        #     messages.error('Error(s) encountered during form processing, please review below and re-submit')
-        # return self.render_to_response(context)
-    # success_url = reverse_lazy('patente:crear_establecimiento')
 
 
 class CrearNatural(CreateView):
@@ -106,21 +63,6 @@ class CrearPatente(CreateView):
     form_class = PatenteForm
     template_name = 'patente/apertura/paso3_patente.html'
     success_url = reverse_lazy('patente:revision_declaracion')
-
-
-# class RevisionDeclaracion(View):
-#     def get(self, request, *args, **kwargs):
-#         try:
-#             template = get_template('patente/apertura/paso4_revision.html')
-#             context = {
-#                 'patente': Patente.objects.last()
-#             }
-#             html = template.render(context)
-#             response = HttpResponse(html)
-#             return response
-#         except:
-#             pass
-#         return HttpResponseRedirect(reverse_lazy('patente:lista_catastro'))
 
 
 class RevisionDeclaracion(TemplateView):
@@ -191,3 +133,10 @@ class ReportEspecie(View):
         except:
             pass
         return HttpResponseRedirect(reverse_lazy('patente:lista_catastro'))
+
+
+class ActualizarDeclaracion(UpdateView):
+    model = Patente
+    form_class = PatenteForm
+    template_name = 'patente/actualizar/paso1_actualizar_declaracion.html'
+    success_url = reverse_lazy('patente:revision_declaracion')
