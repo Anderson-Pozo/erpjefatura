c0d276a05c44b6829335be2ee6cb0cc376257893
Anderson Pozo <imbander092098@gmail.com>
2021-03-04 12:15:59 -0500

    Update model usuario

--- static/js/full_calendar.js
+++ static/js/full_calendar.js
@@ -4,17 +4,33 @@ document.addEventListener('DOMContentLoaded', function () {
 
     var calendar = new FullCalendar.Calendar(calendarEl, {
         locale: 'es',
+        views: {
+            dayGridMonth: { // name of view
+                titleFormat: {year: 'numeric', month: 'long'}
+                // other view-specific options here
+            }
+        },
         initialView: 'dayGridMonth',
         // initialDate: '2021-03-03',
         nextDayThreshold: '00:00:00',
         headerToolbar: {
             right: 'prev,next',
             left: 'title',
-            end: 'dayGridMonth,timeGridWeek'
+            end: 'dayGridMonth'
         },
 
-        events: []
+        events: [
+            {
+                title: "Retrasado",
+                start: "2021-03-01",
+                end: "2021-03-10",
+                description: 'Hola mundo',
+                backgroundColor: '#e03131',
+                borderColor: '#e03131'
+            },
+        ]
     });
+    console.log(calendar);
 
     calendar.render();
 });
