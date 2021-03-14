0cf4b22290726268151678a95ad5b6f3546e4f73
Anderson Pozo <imbander092098@gmail.com>
2021-03-14 18:28:43 -0500

    Import export model persona

--- apps/alcabala/admin.py
+++ apps/alcabala/admin.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
 from .models import Alcabala, Persona, Predio
 
-# Register your models here.
+
+class PersonaResource(resources.ModelResource):
+    class Meta:
+        model = Persona
+
+
+class PersonaAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+    resource_class = PersonaResource
+
+
 admin.site.register(Alcabala)
-admin.site.register(Persona)
 admin.site.register(Predio)
+admin.site.register(Persona, PersonaAdmin)
