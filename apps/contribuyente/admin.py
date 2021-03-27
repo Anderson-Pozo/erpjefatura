225219e53c06e07e1b340505e02c2fedfd1ad2f8
Anderson Pozo <imbander092098@gmail.com>
2021-03-26 21:04:21 -0500

    Admin contribuyente

--- apps/contribuyente/admin.py
+++ apps/contribuyente/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from import_export import resources
 from import_export.admin import ImportExportModelAdmin
-from .models import TipoContribuyente, Natural, Juridico
+from .models import TipoContribuyente, Natural, Juridico, Contribuyente
 
 
 class JuridicoResource(resources.ModelResource):
@@ -22,7 +22,17 @@ class NaturalAdmin(ImportExportModelAdmin, admin.ModelAdmin):
     resource_class = NaturaResource
 
 
+class ContribuyenteResource(resources.ModelResource):
+    class Meta:
+        model = Contribuyente
+
+
+class ContribuyenteAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+    resource_class = ContribuyenteResource
+
+
 admin.site.register(TipoContribuyente)
 admin.site.register(Natural, NaturalAdmin)
 admin.site.register(Juridico, JuridicoAdmin)
+admin.site.register(Contribuyente, ContribuyenteAdmin)
 
