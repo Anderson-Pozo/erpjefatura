104febb5e8ce1cc9248278be0363ff7926baceb7
Anderson Pozo <imbander092098@gmail.com>
2021-05-10 18:34:33 -0500

    Change config database

--- erpjefatura/settings/production.py
+++ erpjefatura/settings/production.py
@@ -1,7 +1,7 @@
 from .base import *
 
 DEBUG = True
-ALLOWED_HOSTS = ['erpjefaturav1.herokuapp.com']
+ALLOWED_HOSTS = ['erpjefatura.herokuapp.com']
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
