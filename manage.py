f1e3f70e6b4ef3a66bd526226a9518781cce579b
Anderson Pozo <imbander092098@gmail.com>
2021-05-10 12:40:32 -0500

    Change production mode

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'erpjefatura.settings.local')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'erpjefatura.settings.production')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
